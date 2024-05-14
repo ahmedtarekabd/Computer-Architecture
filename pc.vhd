1ed385f75d3e3fc50c7b5fb9f4814f9ee65914fe
omar desouki <omar.desouki2@gmai.com>
2024-05-14 05:26:45 +0200

    fetch done just needs more testing

--- pc.vhd
+++ pc.vhd
@@ -5,31 +5,30 @@ USE IEEE.numeric_std.all;
 entity pc is
     port (
         reset : in std_logic;
+        enable : in std_logic;
         clk : in std_logic;
-        pc_out : out std_logic_vector(9 downto 0)
+        pc_out : out std_logic_vector(31 downto 0)
     );
 end entity pc;
 
 architecture pc_behavioral of pc is
-    signal counter : integer := 0;
+    signal counter : unsigned(31 downto 0) := (others => '0');
 begin
     process(clk,reset)
     begin
         if reset = '1' then
-            counter <= 0;
-        -- -- Prevent counter from incrementing on the same clock cycle as reset
-        -- elsif falling_edge(reset) then
-        --         counter <= 0;
-        elsif rising_edge(clk) then
-            if counter = 1024 then
-                counter <= 0;
+            counter <= (others => '0');
+        --only increment counter if enable is high
+        elsif rising_edge(clk) and enable ='1' then
+            if counter = "11111111111111111111111111111111" then 
+                counter <= (others => '0');
             else
                 counter <= counter + 1;
             end if;
         end if;
 
-    end process;
+    end process;    
 
-    pc_out <= std_logic_vector(to_unsigned(counter, 10));
+    pc_out <= std_logic_vector(counter);
 
 end architecture pc_behavioral;
---
+++ test fetch/my_DFF.vhd
@@ -0,0 +1,19 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.all;
+
+ENTITY my_DFF IS
+    PORT( 	d,clk,reset,enable : IN std_logic;
+            q : OUT std_logic);
+END my_DFF;
+
+ARCHITECTURE a_my_DFF OF my_DFF IS
+BEGIN
+    PROCESS(clk,reset)
+    BEGIN
+        IF(reset = '1') THEN
+            q <= '0';
+        ELSIF clk'event and clk = '1' and enable ='1' THEN
+            q <= d;
+        END IF;
+    END PROCESS;
+END a_my_DFF;
