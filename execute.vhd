5e288093a2a6c9ea292534fe70ead743d0ba13cf
Ziad Ghanem <ziad.mahmoud02@eng-st.cu.edu.eg>
2024-05-16 00:52:26 +0300

    Added and tested Execute stage.

--- execute.vhd
+++ execute.vhd
@@ -1,81 +1,114 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
--- no forwarding yet
--- may need to add zero flags to the output ot send to the controller
-
---TODO: add the flags to the output of the execute stage
 
 ENTITY execute IS
     PORT (
-        -------------------------inputs-------------------------
+    -------------------------inputs-------------------------
         clk : IN STD_LOGIC;
-        -- -- pc + 1 propagated
-        -- pc_in : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
-        -- immediate value from decode stage
-        immediate_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- opcode from controller
-        -- operation : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-
-        -- propagated from decode stage
+        ---------------------------- Propagated from the previous stage------------------------
+        -- pc + 1 propagated (32 bits)
+        -- pc propagated
+        pc_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        pc_plus_1_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        -- destination address from decode stage
+        destination_address : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        -- write address 1 and 2
         address_read1_in : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
         address_read2_in : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-        destination_address : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        -- immediate flag 
+        immediate_enable_in : IN STD_LOGIC;
 
-        -- propagated from decode stage
+        ---------------------------- Normal useful inputs ------------------------
+        -- data 1 and 2 from the register file
         data1_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         data2_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- from controller -> to be extended to all of them
-        -- reg_read_control_signals : IN STD_LOGIC;
-        -- branch_control_signals : IN STD_LOGIC;
-        -- sign_extend_control_signals : IN STD_LOGIC;
-        -- decode_Excute_enable : IN STD_LOGIC;
-        -- alu_select_control_signals : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-        -- alu_src2_control_signals : IN STD_LOGIC_vector(1 DOWNTO 0);
-        -- reg_write_control_signals : IN STD_LOGIC;
-        -- --mem
-        -- mem_write_enable_control_signal : IN STD_LOGIC;
-        -- mem_read_enable_control_signal : IN STD_LOGIC;
-        -- sp_control_signal : IN STD_LOGIC;
-        -- from controller
-        control_signals_in : IN STD_LOGIC_VECTOR(22 DOWNTO 0);
 
-        -- -- flags in
-        -- old_negative_flag : in std_logic;
-        -- old_zero_flag : in std_logic;
-        -- old_overflow_flag : in std_logic;
-        -- old_carry_flag : in std_logic;
+        -- immediate value from decode stage
+        immediate_in : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 
-        -- commented out for now
-        -- forwarded stuff
-        -- -- alu to alu forwarding
-        -- alu_result_forward : in std_logic_vector(31 downto 0);
-        -- -- memory to alu forwarding
-        -- memory_result_forward : in std_logic_vector(31 downto 0);
+        ---------------------------- Forwarded data ------------------------
+        -- forwarded data 1 and 2 from Mem stage
+        forwarded_data1_em : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        forwarded_data2_em : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        forwarded_alu_out_em : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        -- forwarded from the write back stage
+        forwarded_data1_mw : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        forwarded_data2_mw : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
 
-        -- -- control signals in that order, 2 for alu, 2 for mem and 2 for wb
-        -- alu_mem_wb_control_signals : in std_logic_vector(5 downto 0);
+        ---------------------------- Forwarding control signals ------------------------
+        -- opp 2 mux selector 3 bits
+        forwarding_mux_selector_op2 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        -- opp 1 mux selector 3 bits
+        forwarding_mux_selector_op1 : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
 
-        -- -- forwarding unit signals
-        -- forwarding_unit_signals : in std_logic_vector(1 downto 0);
+    -------------------------------- Control signals ------------------------
+        
+        -------------------------------- Propagated from the previous stage ------------------------
+        -- 11 bits for memory control signals and 5 bits for write back control signals
+        control_signals_memory_in : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+        control_signals_write_back_in : IN STD_LOGIC_VECTOR(5 DOWNTO 0);
+            -- output them
+        control_signals_memory_out : out STD_LOGIC_VECTOR(10 DOWNTO 0);
+        control_signals_write_back_out : out STD_LOGIC_VECTOR(5 DOWNTO 0);
 
-        -------------------------outputs-------------------------
+        -------------------------------- used in the execute stage ------------------------
+        -- alu selectors 3 bits
+        alu_selectors : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        -- mux selector for src2 and immediate 2 bits
+        alu_src2_selector : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
 
-        -- alu output
-        alu_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- mem and wb control signals
-        -- mem_wb_control_signals_out : OUT STD_LOGIC_VECTOR(22 DOWNTO 0);
-        outputed_control_signals : OUT STD_LOGIC_VECTOR(22 DOWNTO 0);
-        -- addresses
-        address_read1_out : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
-        address_read2_out : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
-        -- data
-        data1_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        data2_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        -- destination address
-        destination_address_out : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
+        -- register enable 1 bit
+        execute_mem_register_enable : IN STD_LOGIC;
+        -- reset signal input
+        RST_signal_input : IN STD_LOGIC;
+        -- Load use reset
+        RST_signal_load_use_input : IN STD_LOGIC;
+        -- E/M flush from exception handling
+        EM_flush_exception_handling_in : IN STD_LOGIC;
+        EM_enable_exception_handling_in : IN STD_LOGIC;
 
-        -- -- pc + 1
-        -- pc_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+------------------------- Outputs -------------------------
+    ------------------------- Propagated outputs -------------------------
+    -- pc + 1 propagated (32 bits)
+    -- pc propagated also used for exception handling
+    pc_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    pc_plus_1_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+    -- destination address from decode stage
+    destination_address_out : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+
+    -- write address 1 and 2 for forwarding and propagation
+    address_read1_out : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+    address_read2_out : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+
+    ---------------------- Stage outputs ----------------------
+
+    -- overflow exception output 1 bit
+    -- zero flag to controller 1 bit
+    -- flag register output 4 bits, 0 -> zero, 1 -> overflow, 2 -> carry, 3 -> negative
+    flag_register_out: OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+    -- ALU output 32 bits
+    alu_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+
+    -- immediate flag output
+    immediate_enable_out : OUT STD_LOGIC;
+
+    ----------------------- For swapping -----------------------
+    -- data 1 and 2 for swapping 32 bits
+    data1_swapping_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    data2_swapping_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+---------------------- Outputs not propagated ----------------------
+    -- 1 bit zero flag
+    -- 1 bit overflow flag
+    -- 3 bits address 1
+    -- 3 bits address 2
+    -- 32 bits PC
+    zero_flag_out_controller : OUT STD_LOGIC;
+    overflow_flag_out_exception_handling : OUT STD_LOGIC;
+    address1_out_forwarding_unit : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+    address2_out_forwarding_unit : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+    pc_out_exception_handling : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
 
     );
 END execute;
@@ -126,116 +159,174 @@ ARCHITECTURE arch_execute OF execute IS
         );
     END COMPONENT my_nDFF;
 
-    SIGNAL flags_temp_in : STD_LOGIC_VECTOR(3 DOWNTO 0);
-    SIGNAL flags_temp_out : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    -- internal signals
+    SIGNAL op1_mux_out : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL op2_mux_out : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL op2_mux_in : STD_LOGIC_VECTOR(31 DOWNTO 0);
 
-    SIGNAL d_internal : STD_LOGIC_VECTOR(127 DOWNTO 0);
-    SIGNAL q_output : STD_LOGIC_VECTOR(127 DOWNTO 0);
+    SIGNAL flag_register_in_temp : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL flag_register_out_temp : STD_LOGIC_VECTOR(3 DOWNTO 0);
 
+    -- for Execute/Mem register
+    SIGNAL d_output : STD_LOGIC_VECTOR(190 DOWNTO 0);
+    SIGNAL q_output : STD_LOGIC_VECTOR(190 DOWNTO 0);
+
+    -- output signals
     SIGNAL alu_out_temp : STD_LOGIC_VECTOR(31 DOWNTO 0);
 
-    SIGNAL alu_select_control_signals : STD_LOGIC_VECTOR(2 DOWNTO 0);
-    SIGNAL alu_src2_control_signals : STD_LOGIC_VECTOR(1 DOWNTO 0);
-    SIGNAL immediate_enable : STD_LOGIC;
-
-    SIGNAL src2 : STD_LOGIC_VECTOR(31 DOWNTO 0); -- src2 can be immediate or register value or 1
+    -- Pipeline registers
+    SIGNAL execute_mem_reset : STD_LOGIC;
+    SIGNAL execute_mem_enable : STD_LOGIC;
 
 BEGIN
+    -- data 2 and immediate mux
+    -- MUX2: mux4x1 PORT MAP (
+    --     inputA => data2_in,
+    --     inputB => immediate_in,
+    --     inputC => "00000000000000000000000000000001",
+    --     inputD => (OTHERS => '0'),
+    --     Sel_lower => alu_src2_selector(0),
+    --     Sel_higher => alu_src2_selector(1),
+    --     output => op2_mux_in
+    -- );
 
-    -- flags_temp_out <= old_negative_flag_temp & old_zero_flag_temp & old_overflow_flag_temp & old_carry_flag_temp;
+    with alu_src2_selector select
+        op2_mux_in <= data2_in when "00",
+        immediate_in when "01",
+        "00000000000000000000000000000001" when "10",
+        (OTHERS => '0') when others;
 
-    alu_select_control_signals <= control_signals_in (18 DOWNTO 16);
-    alu_src2_control_signals <= control_signals_in (15 DOWNTO 14);
-    immediate_enable <= control_signals_in(22);
 
-    -- mux instantiation
-    mux_inst : mux4x1
-    GENERIC MAP(32)
-    PORT MAP(
-        inputA => data2_in, --00
-        inputB => immediate_in, --01
-        inputC => "00000000000000000000000000000001", --10
-        inputD => immediate_in, --11 (can be cahnged later to anythin)
-        Sel_lower => alu_src2_control_signals(0),
-        Sel_higher => alu_src2_control_signals(1),
-        output => src2
-    );
+    -- forwarding mux 1 8x1
+    -- 000 data_1in
+    -- 001 forwarded_alu_out_em
+    -- 010 forwarded_data1_mw
+    -- 011 forwarded_data2_mw 
+    -- 100 forwarded_data1_em
+    -- 101 forwarded_data2_em
+    -- for the rest of the cases, the data is zero
+    with forwarding_mux_selector_op1 SELECT
+        op1_mux_out <= data1_in WHEN "000",
+                        forwarded_alu_out_em WHEN "001",
+                        forwarded_data1_mw WHEN "010",
+                        forwarded_data2_mw WHEN "011",
+                        forwarded_data1_em WHEN "100",
+                        forwarded_data2_em WHEN "101",
+                        (OTHERS => '0') WHEN OTHERS;
 
-    alu_component : ALU
-    PORT MAP(
-        A => data1_in,
-        B => src2, --choosen from the mux
-        opcode => alu_select_control_signals,
+    -- forwarding mux 2 8x1
+    -- 000 data_2in
+    -- 001 forwarded_alu_out_em
+    -- 010 forwarded_data1_mw
+    -- 011 forwarded_data2_mw
+    -- 100 forwarded_data1_em
+    -- 101 forwarded_data2_em
+    -- for the rest of the cases, the data is zero
+    with forwarding_mux_selector_op2 SELECT
+        op2_mux_out <= op2_mux_in WHEN "000",
+                        forwarded_alu_out_em WHEN "001",
+                        forwarded_data1_mw WHEN "010",
+                        forwarded_data2_mw WHEN "011",
+                        forwarded_data1_em WHEN "100",
+                        forwarded_data2_em WHEN "101",
+                        (OTHERS => '0') WHEN OTHERS;
+
+
+    -- ALU
+    ALU1: ALU PORT MAP (
+        A => op1_mux_out,
+        B => op2_mux_out,
+        opcode => alu_selectors,
         F => alu_out_temp,
-        -- NOTE: negative flag is rightmost in testbench waveform
-        negative_flag => flags_temp_in(0),
-        zero_flag => flags_temp_in(1),
-        overflow_flag => flags_temp_in(2),
-        carry_flag => flags_temp_in(3),
-        old_negative_flag => flags_temp_out(0),
-        old_zero_flag => flags_temp_out(1),
-        old_overflow_flag => flags_temp_out(2),
-        old_carry_flag => flags_temp_out(3)
+        zero_flag => flag_register_in_temp(0),
+        overflow_flag => flag_register_in_temp(1),
+        carry_flag => flag_register_in_temp(2),
+        negative_flag => flag_register_in_temp(3),
+        old_negative_flag => flag_register_out_temp(3),
+        old_zero_flag => flag_register_out_temp(0),
+        old_overflow_flag => flag_register_out_temp(1),
+        old_carry_flag => flag_register_out_temp(2)
     );
 
-    -- DFF for the flags
-    flags_dff : my_nDFF GENERIC MAP(4)
+    -- flag register
+    FLAG_REGISTER: my_nDFF
+    GENERIC MAP (4)
+     PORT MAP (
+        Clk => clk,
+        -- check the reset. i decided that it is the user's reset input signal
+        reset => RST_signal_input,
+        enable => '1',
+        d => flag_register_in_temp,
+        q => flag_register_out_temp   
+    );
+
+    -- other outputs that are not sent to register are 
+        -- 1 bit zero flag
+        -- 1 bit overflow flag
+        -- 3 bits address 1
+        -- 3 bits address 2
+        -- 32 bits PC 
+        pc_out_exception_handling <= pc_in;
+        overflow_flag_out_exception_handling <= flag_register_out_temp(1);
+        zero_flag_out_controller <= flag_register_out_temp(0);
+        -- address 1 and 2 forwarding unit
+        address1_out_forwarding_unit <= address_read1_in;
+        address2_out_forwarding_unit <= address_read2_in;
+
+
+
+    d_output <= 
+        control_signals_memory_in & 
+        control_signals_write_back_in & 
+        destination_address & 
+        pc_in & 
+        pc_plus_1_in & 
+        address_read1_in & 
+        address_read2_in & 
+        data1_in & data2_in & 
+        alu_out_temp & 
+        flag_register_out_temp & 
+        immediate_enable_in;
+
+
+    -- Execute/Mem register enable is the execute memory enable  OR propagated immediate flag
+    execute_mem_enable <= execute_mem_register_enable OR immediate_enable_in;
+    -- Execute/Mem register reset is the user input reset signal OR the load use stall signal OR the flush signal from exception handling
+    execute_mem_reset <= RST_signal_input OR RST_signal_load_use_input OR EM_flush_exception_handling_in;
+
+    -- Execute/Mem register
+    execute_mem_reg : my_nDFF GENERIC MAP(191)
     PORT MAP(
-        clk, '0', '1', flags_temp_in, flags_temp_out
+        clk, execute_mem_reset, execute_mem_enable, d_output, q_output
     );
 
-    -- -- output signals + alu out
-    -- pc_out <= pc_in; --removed
-    -- mem_wb_control_signals_out <= control_signals_in;
-    -- address_read1_out <= address_read1_in;
-    -- address_read2_out <= address_read2_in;
-    -- data1_out <= data1_in;
-    -- data2_out <= data2_in;
-    -- destination_address_out <= destination_address;
+-- internal signals to be sent to the next stage output register 188 bits
+    -- 11 bits memory control signals
+    -- 6 bits write back control signals
+    -- 3 bits propagated rdst
+    -- 32 bits propagated pc
+    -- 32 bits propagated pc + 1
+    -- 3 bits propagated write address 1
+    -- 3 bits propagated write address 2
+    -- 32 bits propagated data 1 for swapping
+    -- 32 bits propagated data 2 for swapping
+    -- 32 bits propagated alu out
+    -- 4 bits propagated flags
+    -- 1 bit immediate flag
+    -- 191 bits in total.
+    -- from left to right most significant to least significant
 
-    d_internal <= alu_out_temp & control_signals_in & address_read1_in & address_read2_in & data1_in & data2_in & destination_address;
+    control_signals_memory_out <= q_output(190 DOWNTO 180);
+    control_signals_write_back_out <= q_output(179 DOWNTO 174);
+    destination_address_out <= q_output(173 DOWNTO 171);
+    pc_out <= q_output(170 DOWNTO 139);
+    pc_plus_1_out <= q_output(138 DOWNTO 107);
+    address_read1_out <= q_output(106 DOWNTO 104);
+    address_read2_out <= q_output(103 DOWNTO 101);
+    data1_swapping_out <= q_output(100 DOWNTO 69);
+    data2_swapping_out <= q_output(68 DOWNTO 37);
+    alu_out <= q_output(36 DOWNTO 5);
+    flag_register_out <= q_output(4 DOWNTO 1);
+    immediate_enable_out <= q_output(0);
 
-    execute_mem_reg : my_nDFF GENERIC MAP(128)
-    PORT MAP(
-        clk, '0', immediate_enable, d_internal, q_output
-    );
-
-    alu_out <= q_output(127 DOWNTO 96);
-    outputed_control_signals <= q_output(95 DOWNTO 73);
-    address_read1_out <= q_output(72 DOWNTO 70);
-    address_read2_out <= q_output(69 DOWNTO 67);
-    data1_out <= q_output(66 DOWNTO 35);
-    data2_out <= q_output(34 DOWNTO 3);
-    destination_address_out <= q_output(2 DOWNTO 0);
 END arch_execute;
--- control_signals <= immediate_enable 22
---     & fetch_pc_sel [21 19]
---     & alu_sel [18 16]
---     & alu_src2 [15 14]
---     & alu_register_write 13 -- no need for it
---     & memory_write 12
---     & memory_read 11
---     & memory_stack_pointer [10 9]
---     & memory_address [8 7]
---     & memory_write_data [6 5]
---     & write_back_register_write4
---     & write_back_register_write3
---     & write_back_register_write_data_[2 1]
---     & write_back_register_write_address_1; 0
--- forwarding_unit : forwarding_unit
--- port map(
---     forwarding_unit_signals => forwarding_unit_signals,
---     address_read1_in => address_read1_in,
---     address_read2_in => address_read2_in,
---     data1_in => data1_in,
---     data2_in => data2_in,
---     alu_result_forward => alu_result_forward,
---     memory_result_forward => memory_result_forward,
---     alu_mem_wb_control_signals => alu_mem_wb_control_signals,
---     address_read1_out => address_read1_out,
---     address_read2_out => address_read2_out,
---     data1_out => data1_out,
---     data2_out => data2_out,
---     destination_address_out => destination_address_out,
---     mem_wb_control_signals => mem_wb_control_signals
--- );
